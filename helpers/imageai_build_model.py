d1b09ae77a918a104ed880108c4d3c7418a79762
HuManChu <philipp.ausserlechner@gmail.com>
2019-01-12 16:56:50 +0100

    still not working

--- helpers/imageai_build_model.py
+++ helpers/imageai_build_model.py
@@ -2,4 +2,4 @@ from imageai.Prediction.Custom import ModelTraining
 model_trainer = ModelTraining()
 model_trainer.setModelTypeAsResNet()
 model_trainer.setDataDirectory("../data/images")
-model_trainer.trainModel(num_objects=4, num_experiments=100, enhance_data=True, batch_size=32, show_network_summary=True)
+model_trainer.trainModel(num_objects=2, num_experiments=2, enhance_data=True, batch_size=1, show_network_summary=True)
