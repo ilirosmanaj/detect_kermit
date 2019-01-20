503523bf685b6a497c5f38818d754a23b24dbc93
HuManChu <philipp.ausserlechner@gmail.com>
2019-01-20 16:17:37 +0100

    Fix detection from video. Updated README

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
-# Kermit Detection repo
+# Kermit Detection Repo
 
 This repository contains the code for detecting Kermit (the frog) from
 the puppets tv show. It uses [ImageAI](https://github.com/OlafenwaMoses/ImageAI) python library
 (which is build on top of tensorflow object detection API).
 
 
-# Installation and running
+# Setup
 
 **Note**: The following has only been tested on linux operating systems,
 so not sure whether it will work for other operation systems.
@@ -14,13 +14,6 @@ Install the needed packaged via pip, by running:
 
 ```bash
 pip install -r requirements.txt
-```
-
-pip distribution of tensorflow does not work for osX, so it can be installed via the url
-using the following command:
-
-```
-python3 -m pip install --upgrade https://storage.googleapis.com/tensorflow/mac/cpu/tensorflow-1.12.0-py3-none-any.whl
 ```
 
 Since models are growing quite big, we are using Git LFS (Large File Storage). In order to install
@@ -36,15 +29,126 @@ ubuntu:
 sudo apt-get install git-lfs
 ```
 
+
 ## General steps:
 
 * Get images from `The muppets show` videos
 * Use frames from episode 1 and 2 as training set
     * Those frames are divided in two classes: `kermit` and `no-kermit`
-* Use frames from episode 3 as testing set
+* Use frames from episode 3 as validation set
 * Enrich kermit images by:
-    * rotating images
+    * rotating images (left, right and 180 degrees)
     * downloading some kermit images from google
+* Enrich non kermit images by adding non kermit images (e.g. green frogs, human, nature etc)
 
-Good to have:
-    * some automated way of seeing results from evaluation the model
+## Running
+
+### Get images from vidoes
+
+In order to get the frames as images from video, run the following:
+
+```bash
+cd helpers
+python convert_vid2image.py
+```
+
+**Note:** Due to large file sizes the video files are not checked in in the repository
+
+### Rotate Images
+
+In order to learn the model better, its a common practice to enrich the dataset with 
+rotated images so that it sees things in different perspectives.
+
+To do this run the following:
+
+```bash
+cd helpers
+python rotate_images.py
+```
+
+This will store all images in the same path with `right` or `left` postfix.
+
+### Download from google
+
+As part of enriching the data, model is supplied with some images outside of 
+the served images from the video. What was done here is using the `google_images_download` which
+can be found [here](https://github.com/hardikvasa/google-images-download)  
+
+
+```bash
+cd helpers
+python downloads_from_google.py
+```
+
+### Training the model
+
+Since ImageAI library was used, we have trained the CustomImagePrediction model (which uses Resnets as 
+network model type).
+
+To train the model, run the following:
+
+```bash
+python imageai_build_model.py
+```
+
+**Note:** Please make sure that the folder structure for images is as following:
+
+* data
+    * images
+        * train
+            * kermit
+            * no-kermit
+        * test
+            * kermit
+            * no-kermit
+            
+ After model is trained, the corresponding trained model is stored in `h5` format under 
+ the `data/images/models/some_name.h5`
+
+### Run predictions on the model
+
+After having the model trained, you can run predictions on it using two different input formats
+(either a video or image).
+
+To run predictions on an image, run the following:
+
+```bash
+python kermit_model_evaluation.py -t (one of image or video) -f (file path to image - comma 
+separated string supported as well, or path to video)
+```
+
+E.g. Predicting an image:
+
+```bash
+python kermit_model_evaluation.py -t image -f kermit.jpeg
+
+Predicting the kermit.jpeg image
+ kermit: 99.87 no-kermit: 0.13
+
+```
+...which is awesome.
+
+Same can be done for a video:
+
+```bash
+python kermit_model_evaluation.py -t video -f MuppetsEpisode3.avi
+```
+
+This will get all the frames in 1 second interval from the video, store them under `tmp` folder
+as jpegs with a text on top of the image that shows the prediction result for each frame. 
+
+
+## Troubleshooting:
+
+pip distribution of tensorflow does not work for osX, so it can be installed via the url
+using the following command:
+
+```
+python3 -m pip install --upgrade https://storage.googleapis.com/tensorflow/mac/cpu/tensorflow-1.12.0-py3-none-any.whl
+```
+
+Sometimes tensorflow gpu might complain about running devices. To fix it, just set the `CUDA_VISIBLE_DEVICES` as follows:
+
+```
+export CUDA_VISIBLE_DEVICES=''
+```
--- kermit_model_evaluation.py
+++ kermit_model_evaluation.py
@@ -1,6 +1,5 @@
 import argparse
 import asyncio
-from copy import deepcopy
 from typing import Union
 
 import os
@@ -14,85 +13,75 @@ from helpers.utils import print_progress, gather_dict
 EXECUTION_PATH = os.getcwd()
 
 
-async def predict_image(image_name: Union[str, np.ndarray], model: CustomImagePrediction, input_type: str) -> str:
+async def predict_image(image_name: Union[str, np.ndarray], model: CustomImagePrediction) -> str:
     """Predicts a given image with the supplied prediction model"""
-    if input_type == 'file':
-        print('\nPredicting the {} image'.format(image_name))
+    print('\nPredicting the {} image'.format(image_name))
 
-    image = os.path.join(EXECUTION_PATH, image_name) if isinstance(image_name, str) else image_name
-
-    predictions, probabilities = model.predictImage(image, result_count=2, input_type=input_type)
+    predictions, probabilities = model.predictImage(os.path.join(EXECUTION_PATH, image_name), result_count=2)
 
     representation = ''
     for eachPrediction, eachProbability in zip(predictions, probabilities):
-        representation += '     {} : {}'.format(eachPrediction, eachProbability)
+        representation += ' {0}: {1:.2f}'.format(eachPrediction, float(eachProbability))
 
     return representation
 
 
 async def predict_video(video_path: str, model: CustomImagePrediction):
     cap = cv2.VideoCapture(video_path)
-    VIDEO_PLAYING_SPEED = 2
     VIDEO_DURATION_IN_SECONDS = int(int(cap.get(cv2.CAP_PROP_FRAME_COUNT)) / cap.get(cv2.CAP_PROP_FPS)) + 1
+    # remove tmp images
+    # os.system('rm -rf tmp')
 
-    print('Predicting video frames...')
+    print('Gathering frames from the video...')
 
     counter = 0
     tasks = {}
     # read all frames and run predictions on them
-
     while cap.isOpened():
         # set position to only read full seconds
         cap.set(cv2.CAP_PROP_POS_MSEC, (counter * 1000))
         ret, frame = cap.read()
 
-        tasks[counter] = asyncio.ensure_future(predict_image(deepcopy(frame), model, input_type='array'))
+        if not ret:
+            break
+
+        # store the image and use that one for predicting
+        image_name = 'tmp/test_frame{}.jpg'.format(counter)
+        cv2.imwrite(image_name, frame)
+
+        tasks[image_name] = asyncio.ensure_future(predict_image(image_name, model))
         counter += 1
         print_progress(counter / VIDEO_DURATION_IN_SECONDS)
 
-        if counter >= VIDEO_DURATION_IN_SECONDS:
-            break
-
-    print('\nGetting predictions foreach frame from the model')
-    results = await gather_dict(tasks)
-
-    print('\nStarting video output....')
-
-    # show video with the given results
-    cap = cv2.VideoCapture(video_path)
-    counter = 0
-    while cap.isOpened():
-        cap.set(cv2.CAP_PROP_POS_MSEC, counter)
-        ret, frame = cap.read()
-        cv2.imshow('Test', frame)
-
-        index = int(counter/1000)
-        res = results.get(index)
-
-        if res:
-            print(results[index])
-            results.pop(index)
-
-        counter += 1
-
-        if cv2.waitKey(VIDEO_PLAYING_SPEED) == ord('q'):
-            break
-
     cap.release()
     cv2.destroyAllWindows()
+
+    print('\nGetting predictions foreach frame from the model...')
+    results = await gather_dict(tasks)
+
+
+    print('\nWriting predictions on images....')
+
+    for image_path in results.keys():
+        print('\nWriting prediction for {}'.format(image_path))
+        img = cv2.imread(image_path)
+        cv2.putText(img, results[image_path], (130, 25), cv2.FONT_HERSHEY_PLAIN, 1.5, (0, 0, 255), 1, cv2.LINE_AA)
+        cv2.imwrite(image_path, img)
+
+    print('\nDone....')
 
 
 async def main(file_type: str, files: str):
     model = CustomImagePrediction()
     model.setModelTypeAsResNet()
 
-    model.setModelPath(os.path.join(EXECUTION_PATH, 'data/images/models/model_ex-100_acc-0.875000.h5'))
+    model.setModelPath(os.path.join(EXECUTION_PATH, 'data/images/models/kermit_finder.h5'))
     model.setJsonPath(os.path.join(EXECUTION_PATH, 'data/images/json/model_class.json'))
     model.loadModel(num_objects=2)  # number of objects on your trained model
 
     if file_type == 'image':
         for image in files.split(','):
-            await predict_image(image_name=image, model=model, input_type='file')
+            print(await predict_image(image_name=image, model=model))
     else:
         await predict_video(video_path=files, model=model)
 
@@ -101,7 +90,7 @@ if __name__ == '__main__':
     arguments = argparse.ArgumentParser()
     arguments.add_argument('--file_type', '-t', help='Inputs file type', type=str, default='video')
     arguments.add_argument('--files', '-f', help='File path. Comma separated images accepted if type image',
-                           type=str, default='Muppets.avi')
+                           type=str, default='MuppetsEpisode3.avi')
     args = arguments.parse_args()
 
     loop = asyncio.get_event_loop()
