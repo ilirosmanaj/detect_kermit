759c6573966de834f1aa77926aa9dd15ee362f56
HuManChu <philipp.ausserlechner@gmail.com>
2019-01-20 20:07:33 +0100

    Remove unused images

--- helpers/convert_vid2image.py
+++ helpers/convert_vid2image.py
@@ -1,1 +1,16 @@
-# TODO add the code for converting videos
+import cv2
+
+count = 0
+cap = cv2.VideoCapture('data/video.avi')
+ret, image = cap.read()
+
+print('Starting getting frames from video...')
+while ret:
+    cap.set(cv2.CAP_PROP_POS_MSEC, (count * 1000))
+    ret, image = cap.read()
+    if not ret:
+        break
+
+    cv2.imwrite( 'data/videoframe/' + "frame%d.jpg" % count, image)
+    count = count + 1
+
